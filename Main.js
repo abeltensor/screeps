4fdd2823b979a0423a64a99fb5b13683b77f309c
Jay king <abeltensor@gmail.com>
2016-11-16 22:46:32 -0500

    added autospawn

--- Main.js
+++ Main.js
@@ -1,10 +1,10 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-
     for(var name in Memory.creeps) {
-        if(!Game.creeps[name]) {
+    if(!Game.creeps[name]) {
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
@@ -18,6 +18,21 @@ module.exports.loop = function () {
         console.log('Spawning new harvester: ' + newName);
     }
 
+    var tower = Game.getObjectById('af8f513ff636da0d85592624');
+    if(tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
@@ -25,6 +40,9 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }
